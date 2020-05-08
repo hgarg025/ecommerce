bdb3526e18e7e223b0a1798f6c32f5d4a46ec9a7
harshit garg <hgarg_be17@thapar.edu>
2020-05-08 20:11:10 +0530

    slider completed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,26 @@
-import React , {useContext} from 'react';
+import React , {useContext, useState} from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 import { Slider } from 'react-native-elements';
 import ProductList from '../components/ProductList';
 
 const HomeScreen = () => {
 
+	const [value, setValue] = useState(0.5);
+
     return(
     <View style={styles.basic} >
     <ProductList title="Products" />
     <ProductList title="Sold" />
     <ProductList title="Orders" />
-    <Slider style={styles.slider} />
+    <Slider style={styles.slider} 
+    maximumValue={5}
+    minimumValue={0}
+    step={0.5}
+    value={value}
+    onValueChange={(val) => setValue(val)}
+    />
+    <Text style={styles.value}>Delivery Distance</Text>
+    <Text style={styles.value}>{value} km</Text>
     </View>
 	);
 }
@@ -25,6 +35,11 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'space-between',
 		marginBottom: 100
+	},
+	value : {
+		textAlign : 'center',
+		fontSize : 20,
+		fontWeight : "900"
 	}
 
 });
