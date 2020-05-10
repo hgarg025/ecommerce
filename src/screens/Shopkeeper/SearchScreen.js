20390bb154a3482eca7c590c548b4010d20ddb75
Pulkit Gupta <pulkitgupta64@gmail.com>
2020-05-10 21:05:28 +0530

    Added Styling

--- src/screens/Shopkeeper/SearchScreen.js
+++ src/screens/Shopkeeper/SearchScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 import { Slider } from 'react-native-elements';
-import ProductList from '../components/ProductList';
+import ProductList from '../../components/ProductList';
 
 const SearchScreen = () => {
     return(
